d4412a3716529d4c813d9e3bf4ec74b7e8e63c79
Yuval Greenfield <ubershmekel@gmail.com>
2018-10-02 16:59:49 -0700

    Code formatting

--- experiment-run/mnist_cnn.py
+++ experiment-run/mnist_cnn.py
@@ -1,7 +1,6 @@
-'''Trains a simple convnet on the MNIST dataset.
+'''
+Trains a simple convnet on the MNIST dataset.
 
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
 16 seconds per epoch on a GRID K520 GPU.
 '''
 
@@ -14,7 +13,7 @@ from keras.layers import Conv2D, MaxPooling2D
 
 batch_size = 128
 num_classes = 10
-epochs = 1
+epochs = 4
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -46,9 +45,7 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 #model.add(Conv2D(32, (3, 3), activation='relu'))
 #model.add(MaxPooling2D(pool_size=(2, 2)))
 #model.add(Dropout(0.25))
@@ -57,15 +54,19 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+model.compile(
+    loss=keras.losses.categorical_crossentropy,
+    optimizer=keras.optimizers.Adadelta(),
+    metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+model.fit(
+    x_train,
+    y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    verbose=1,
+    validation_data=(x_test, y_test))
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
